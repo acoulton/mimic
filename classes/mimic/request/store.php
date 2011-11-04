3f908a0d5b93f2ea6e7522611fdfa3e7e4dc40d9
Andrew Coulton <andrewnfcoulton@gmail.com>
2011-11-04 11:09:23 +0000

    Cannot record a request that has not been executed

--- classes/mimic/request/store.php
+++ classes/mimic/request/store.php
@@ -28,8 +28,20 @@ class Mimic_Request_Store
 		
 	}
 	
+	/**
+	 * Record the request (and its response) in the appropriate index and
+	 * body files for replaying at a future date.
+	 * 
+	 * @param Request $request 
+	 * @throws Mimic_Exception_NotExecuted If the request has not been executed
+	 */
 	public function record(Request $request)
 	{
-		
+		if ($request->response() === NULL)
+		{
+			throw new Mimic_Exception_NotExecuted(
+				'Could not record the :method request to :uri because the request has not been executed', 
+					array(':method'=>$request->method(),':uri'=>$request->uri()));
+		}
 	}
 }
