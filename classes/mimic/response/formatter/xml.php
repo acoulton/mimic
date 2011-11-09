8c2db86542e41994e5782a73ef21a182338111d6
Andrew Coulton <andrewnfcoulton@gmail.com>
2011-11-09 17:17:26 +0000

    Convert XML responses to human-readable format for storage

--- classes/mimic/response/formatter/xml.php
+++ classes/mimic/response/formatter/xml.php
@@ -12,4 +12,23 @@
 class Mimic_Response_Formatter_XML extends Mimic_Response_Formatter
 {
 	protected $_extension = '.xml';
+	
+	/**
+	 * Converts XML to a "pretty" format prior to saving, for ease of editing.
+	 * @param string $path
+	 * @param string $file_prefix
+	 * @param string $content
+	 * @return string 
+	 */
+	public function put_contents($path, $file_prefix, $content)
+	{
+		$xml_doc = new DOMDocument;		
+		if ($xml_doc->loadXML($content, LIBXML_NOERROR | LIBXML_NOWARNING))
+		{
+			$xml_doc->formatOutput = TRUE;
+			$content = $xml_doc->saveXML();
+		}		
+		return parent::put_contents($path, $file_prefix, $content);
+	}
+
 }
--- tests/mimic/response/formatter/XMLTest.php
+++ tests/mimic/response/formatter/XMLTest.php
@@ -17,4 +17,32 @@ class Mimic_Response_Formatter_XMLTest extends Mimic_Response_FormatterBaseTest 
 	
 	protected $_formatter_class_name = 'Mimic_Response_Formatter_XML';
 	protected $_expect_extension = '.xml';
+	
+	public function provider_should_prettify_XML_data()
+	{	
+		return array(
+			array(
+				'<?xml version="1.0"?><xs:schema xmlns:xs="http://www.w3.org/2001/XMLSchema"><xs:element name="note"><xs:complexType><xs:sequence><xs:element name="to" type="xs:string"/><xs:element name="from" type="xs:string"/><xs:element name="heading" type="xs:string"/><xs:element name="body" type="xs:string"/></xs:sequence></xs:complexType></xs:element></xs:schema>',
+			)			
+		);
+	}
+	
+	/**
+	 * @dataProvider provider_should_prettify_XML_data
+	 * @param array $input_data 
+	 */
+	public function test_should_prettify_XML_data($response_text)
+	{		
+		
+		// Store the data
+		$formatter = new Mimic_Response_Formatter_XML;
+		$formatter->put_contents(vfsStream::url('responses/'), 'xml_test', $response_text);
+		
+		// Load the data		
+		$xml_text = file_get_contents(vfsStream::url('responses/xml_test.xml'));
+		
+		$this->assertXmlStringEqualsXmlString($response_text, $xml_text);
+		$this->assertNotEquals($response_text, $xml_text);
+	}
+
 }
